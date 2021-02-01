435bab9c1032f1d7c06a734868e01e5a978a9db7
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-02-01 11:49:59 +0530

    Python oops

---
+++ myclass.py
@@ -0,0 +1,10 @@
+class UserInfo:
+    def __init__(self,name,empid):
+        self.name=name
+        self.empid=empid
+    def setAddress(self):
+        print(self.name)
+        print(self.empid)
+    def showDetails(self):
+        print(self.name,'empid is ',self.empid)   
+        
